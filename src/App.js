581ec17bd4b86f7cb5fdc7c6a9c949f4bc10b098
Wesckley <wesckley.gomes@gmail.com>
2018-03-19 20:23:01 -0300

    adicionando firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MessageList from './components/MessageList';
+import firebase from 'firebase';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    var config = {
+      apiKey: "AIzaSyCKHYIj2aSGOkzCEJFgWWD1J9pM1CF0PY0",
+      authDomain: "iot-teste-5da47.firebaseapp.com",
+      databaseURL: "https://iot-teste-5da47.firebaseio.com",
+      storageBucket: "iot-teste-5da47.appspot.com",
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +27,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <MessageList db={firebase}/>
+        </div>
       </div>
+
     );
   }
 }
---
+++ src/components/MessageList.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			messages: []
+		}
+		this.readData();
+	}
+
+	readData(){
+		return this.props.db.database().ref('/messages').once('value').then(function(snapshot) {
+		  //var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+		  alert(snapshot.val());
+		  
+		});
+	}
+
+	render (){
+		return (
+			<div>
+				MessageList Component
+			</div>
+
+			
+		)
+	}
+}    
+
+export default MessageList
